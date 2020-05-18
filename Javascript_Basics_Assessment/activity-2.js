751125de33d7f6fd9a44505827189257c2a0a22f
Sarah Price <48076969+SarehPrice@users.noreply.github.com>
2020-05-17 20:37:31 -0500

    Finished

--- Javascript_Basics_Assessment/activity-2.js
+++ Javascript_Basics_Assessment/activity-2.js
@@ -3,8 +3,8 @@
 // Otherwise, alert that their name is less than four characters.
 
 
-var name = prompt("How many letters are in your name?");
-if (name > 4) {
+var name = prompt("What is your name?");
+if (name.length > 4) {
 alert("Your name is greater than 4 characters");
 } else {
 alert("Your name is less than 4 characters");
